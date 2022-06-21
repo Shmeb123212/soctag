629f149bed0268dcc9891cc076795a7b4a7906a5
Shmeb <1gledpobgornuk@gmail.com>
2022-06-21 12:43:06 +0300

    main

--- ajax.php
+++ ajax.php
@@ -1,17 +1,31 @@
-<?php
 
-/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
-где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
 
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
-$token = "5425079508:AAGixtGLBVGR0m-4vQjaoeTpWZ5e7atwK54";
-$chat_id = "-614870805";
+
+<? // #начало Обработчик данных
+header("Content-Type: text/html; charset=UTF-8");
+if( $_POST['user_name']) { $name = htmlspecialchars ( $_POST['user_name']); }
+if($_POST['user_phone']) { $phone = htmlspecialchars ($_POST['user_phone']); }
+if($_POST['user_email']) { $mail = htmlspecialchars ($_POST['user_email']); }
+if($_POST['user_item']) { $item = htmlspecialchars ($_POST['user_item']); }
+
+// $name = $_POST['user_name'];
+// $phone = $_POST['user_phone'];
+// $email = $_POST['user_email'];
+// $title = $_POST['user_item'];
+
+$site = $_SERVER['SERVER_NAME'];
+
+$token = "5451738053:AAFGDGOFbcQwZaWkPQrPyhN3Sj3WSHnA1ng";
+$chat_id = "-761043071";
+$date = date('d.m.Y H:i');
+
 $arr = array(
-  'Имя пользователя: ' => $name,
-  'Телефон: ' => $phone,
-  'Email' => $email
+	'Заявка с сайта: ' => $site,
+	'Имя: ' => $name,
+    'Телефон: ' => $phone,
+	'Почта: ' => $main,
+	'Услуга: ' => $item,
+	'Дата: ' => $date
 );
 
 foreach($arr as $key => $value) {
@@ -20,9 +34,32 @@ foreach($arr as $key => $value) {
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-if ($sendToTelegram) {
-  header('Location: thank-you.html');
-} else {
-  echo "Error";
-}
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8" />
+	<meta name='robots' content='noindex,follow' />
+	<title>Ваш заказ принят!</title>
+	<link rel="stylesheet" href="cssl/style.css" />
+	<style>
+		#senks_block {color: #313E47;text-align: center;position: fixed;top: 10%;width: 100%;}
+		#senks_block img {width: 185px;margin-bottom: 10px;}
+		#senks_block h1 {font-size: 36px;font-weight: 700;text-transform: uppercase;color: rgba(9, 14, 100, 0.7);}
+		.senks_text {line-height: 1.2;font-size: 18px;margin: 25px auto;}
+		.senks_red {color: #fff;font-size: 19px;font-weight: bold;background: rgba(9, 14, 100, 0.7);height: 45px;line-height: 45px;}
+	</style>
+ 
+</head>
+<body style="background-size: 100% 100%;">
+	<div id="senks_block">
+		<img src="index.png" alt="">
+		<h1><? if(isset($name)){echo $name;} ?><br>Ваш заказ принят!</h1>
+	
+		<p class='senks_red'>Пожалуйста<? if(isset($name)){echo " ".$name;} ?>, дождитесь звонка </p>
+	</div>
+</body>
+</html>
--- scripts/basket.js
+++ scripts/basket.js
@@ -4,18 +4,33 @@
 
     function getBtnKatalog () {
         let allBtns = document.getElementsByClassName('table__item-btn')
+        let allTitle = document.getElementsByClassName('table__item-title')
         let modal = document.getElementsByClassName('modal')
             allBtns = [...allBtns]
             modal = [...modal]
+            allTitle = [...allTitle]
+        let numberBtn = 0
         allBtns.map((e,i)=>{
             e.addEventListener('click', ()=>{
+                numberBtn = i
                 modal[0].classList.toggle('active-modal')
+                let hiddenInput = modal[0].querySelector('.modal__input-hidden')
+                hiddenInput.textContent = allTitle[numberBtn].textContent
+                console.log(hiddenInput);
             })
         })
+
+
 
         modal[0].addEventListener('click', ()=>{
         //    
         })
+
+        modal[0].querySelector('.modal__exit').addEventListener('click', (e)=>{
+            modal[0].classList.remove('active-modal')
+        })
+
+       
 
         modal[0].querySelector('.btn__modal').addEventListener('click', (e)=>{
             modal[0].classList.remove('active-modal')
